ddc677b53f8cccf7d34f20a45d3cda4d3bad4e59
hampus.mann <hampus.mann@elev.ga.ntig.se>
2019-11-19 09:23:28 +0100

    JavDoc tillagt

--- src/Clock.java
+++ src/Clock.java
@@ -3,12 +3,20 @@ public class Clock {
     private NumberDisplay minutes;
     private String displayString;
 
+    /**
+     * Constructs a new Clock
+     */
     public Clock() {
         hours = new NumberDisplay(24);
         minutes = new NumberDisplay(60);
         updateDisplay();
     }
 
+    /**
+     * Constructs a Clock
+     * @param hour sets the hours
+     * @param minute sets the minutes
+     */
     public Clock(int hour, int minute) {
         hours = new NumberDisplay(24);
         hours.setValue(hour);
@@ -17,6 +25,9 @@ public class Clock {
         updateDisplay();
     }
 
+    /**
+     * Increases the value of the clock
+     */
     public void timeTick() {
         if (minutes.getValue() == 59) {
             hours.increment();
@@ -27,16 +38,28 @@ public class Clock {
         updateDisplay();
     }
 
+    /**
+     * Sets the time
+     * @param hour sets the hours
+     * @param minute sets the minutes
+     */
     public void setTime(int hour ,int minute) {
         hours.setValue(hour);
         minutes.setValue(minute);
         updateDisplay();
     }
 
+    /**
+     * Returns the Clock value
+     * @return
+     */
     public String getTime() {
         return displayString;
     }
 
+    /**
+     * Updates the displayString value
+     */
     private void updateDisplay() {
         displayString = hours.getDisplayValue() + ":" + minutes.getDisplayValue();
     }
--- src/NumberDisplay.java
+++ src/NumberDisplay.java
@@ -2,21 +2,37 @@ public class NumberDisplay {
     private int limit;
     private int value;
 
+    /**
+     * Constructs a NumberDisplay
+     * @param maxLimit limits the NumberDisplay
+     */
     public NumberDisplay (int maxLimit) {
         this.limit = maxLimit;
         this.value = 0;
     }
 
+    /**
+     *
+     * @return returns value
+     */
     public int getValue() {
         return value;
     }
 
+    /**
+     * set a new value for Value
+     * @param newValue a new value
+     */
     public void setValue(int newValue) {
         if (newValue > 0 && newValue < limit) {
             this.value = newValue;
         }
     }
 
+    /**
+     * Returns the value of the NumberDisplay
+     * @return
+     */
     public String getDisplayValue() {
         String display = null;
         if (value > 9) {
@@ -27,6 +43,9 @@ public class NumberDisplay {
         return display;
     }
 
+    /**
+     *  Increases the value of value by 1
+     */
     public void increment() {
         value++;
         if (value >= limit) {
